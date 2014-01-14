b033c631e5e5b270256127401c1a59faac66c01d
Paulo Amaral <paulo.amaral@gmail.com>
2014-01-14 00:55:30 -0200

    Create 10_utf8.sh
    This file enables UTF-8 in putty

---
+++ bash.d/10_utf8.sh
@@ -0,0 +1,3 @@
+# Enable UTF-8 in PuTTY (from http://www.earth.li/~huggie/blog/tech/mobile/putty-utf-8-trick.html).
+# The \e%G sets UTF-8 mode; \e[?47h and \e[?47l switch to the alternative screen and back.
+echo -ne '\e%G\e[?47h\e%G\e[?47l'
