d7f8cc9f8a0590120926dce7b35f8175711d802d
Paulo Amaral <paulo.amaral@gmail.com>
2014-09-02 09:51:37 +0100

    Update 02_alias.sh
    created vim alias

--- bash.d/02_alias.sh
+++ bash.d/02_alias.sh
@@ -6,9 +6,12 @@
 alias ll='ls -lh --group-directories-first --time-style=long-iso'
 alias ls='ls --color=always'
 
-# Dir
+# mkdir - create subdirs automatically
 alias mkdir='mkdir -p'
 
-# ff (find in files): usa grep --color=always para destacar o texto procurado
+# ff (find in files): uses grep --color=always to highlight the searched text
 alias ff='grep --color=always -B2 -F2 -i -n -H -a'
+
+#vim - using custom configuration file
+vim='vim -u $FILESROOT/vimrc'
 
