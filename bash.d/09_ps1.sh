424e89127d4e862f08b1079634f9a37f7e68a305
paulera <paulo@Blinky.wf.local>
2015-09-22 15:01:05 +0100

    Changes root verification

--- bash.d/09_ps1.sh
+++ bash.d/09_ps1.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-ID=$(id -u)
-ID="_"$ID
-
 
 JOBS="if [ ! \$(jobs | wc -l) == 0 ]; then echo \[\e[01\;30m\]\\\[\$(jobs | wc -l)\\\]\\[\e[0m\]\ ; fi"
 FACE="if [ ! \$? = 0 ]; then echo \[\e[01\;31m\][!]\[\e[0m\]\ ; fi;"
 
-if [ "$ID" != "_0" ]; then
-	# comum
-	export PS1="\`$FACE\`\`$JOBS\`\[\e[1;32m\]\u@\h \[\e[1;34m\]\w \$ \[\e[0m\]";
-else
+if [ "_$(id -u)" == "_0" ]; then
 	# root
 	export PS1="\[\e[1;31m\]\u@\h \[\e[1;34m\]\w # \[\e[0m\]";
+else
+	# common
+	export PS1="\`$FACE\`\`$JOBS\`\[\e[1;32m\]\u@\h \[\e[1;34m\]\w \$ \[\e[0m\]";	
 fi
