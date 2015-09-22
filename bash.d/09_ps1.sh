b3fba04c1f028987eca04a10fd6bdc43841db4db
U-WF\paulo <paulo@Blinky.wf.local>
2015-09-22 14:16:40 +0100

    sad face when last command is an error

--- bash.d/09_ps1.sh
+++ bash.d/09_ps1.sh
@@ -4,7 +4,7 @@ ID="_"$ID
 
 if [ "$ID" != "_0" ]; then
 	# comum
-	export PS1="\[\e[1;32m\]\u@\h \[\e[1;34m\]\w \$ \[\e[0m\]";
+	export PS1="\`if [ ! \$? = 0 ]; then echo \[\e[01\;31m\]x_x\[\e[0m\]\ ; fi\`\[\e[1;32m\]\u@\h \[\e[1;34m\]\w \$ \[\e[0m\]";
 else
 	# root
 	export PS1="\[\e[1;31m\]\u@\h \[\e[1;34m\]\w # \[\e[0m\]";
