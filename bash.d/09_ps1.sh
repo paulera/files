c11e2837b159b97028de717f508335d792bac561
paulera <paulo@Blinky.wf.local>
2015-09-22 14:52:05 +0100

    running jobs

--- bash.d/09_ps1.sh
+++ bash.d/09_ps1.sh
@@ -2,9 +2,13 @@
 ID=$(id -u)
 ID="_"$ID
 
+
+JOBS="if [ ! \$(jobs | wc -l) == 0 ]; then echo \[\e[01\;30m\]\\\[\$(jobs | wc -l)\\\]\\[\e[0m\]\ ; fi"
+FACE="if [ ! \$? = 0 ]; then echo \[\e[01\;31m\][!]\[\e[0m\]\ ; fi;"
+
 if [ "$ID" != "_0" ]; then
 	# comum
-	export PS1="\`if [ ! \$? = 0 ]; then echo \[\e[01\;31m\]x_x\[\e[0m\]\ ; fi\`\[\e[1;32m\]\u@\h \[\e[1;34m\]\w \$ \[\e[0m\]";
+	export PS1="\`$FACE\`\`$JOBS\`\[\e[1;32m\]\u@\h \[\e[1;34m\]\w \$ \[\e[0m\]";
 else
 	# root
 	export PS1="\[\e[1;31m\]\u@\h \[\e[1;34m\]\w # \[\e[0m\]";
