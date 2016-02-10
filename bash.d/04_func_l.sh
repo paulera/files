643ab715dbc42aac07c025375b00c6ed702ce885
paulera <paulo@Blinky.wf.local>
2016-02-10 11:03:12 +0000

    Fixed 'l' file listing function

--- bash.d/04_func_l.sh
+++ bash.d/04_func_l.sh
@@ -1,5 +1,5 @@
 # minimal file listing
 l() {
-	ls -lh --group-directories --color=always --time-style=long-iso $* | awk "{print \$1, \$3, \$4, \$8, \$9, \$10;}" | grep -v ^total$
+	ls -lh --group-directories --color=always --time-style=long-iso $* | awk "{print \$1, \$3, \$4, \$8, \$9, \$10, \$11;}" | grep -v ^total$
 }
 
