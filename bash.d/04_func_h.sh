217c71b379efe3ba68bba9503063920e796abe79
Paulo Amaral <paulo.amaral@gmail.com>
2016-09-14 11:12:29 +0100

    added a different color to grep in h command, so grep can be reused over it highlighting in red

--- bash.d/04_func_h.sh
+++ bash.d/04_func_h.sh
@@ -1,6 +1,3 @@
-
-
-
 #!/bin/bash
 
 # search history
@@ -9,12 +6,12 @@ function h() {
 
 	HIGHLIGHT=`echo -e '\033[1;34m'`
 	NORMAL=`echo -e '\033[0m'`
-
+	
     if [ -z $1 ]; then
         echo "Search the commands history"
         echo "Usage: h <search terms>"
     else
-       history | grep --color=always $* | sed -e "s/^ \([0-9]\+\)/\n $HIGHLIGHT\1$NORMAL/g"
+       history | GREP_COLOR="1;32" grep -i --color=always $* | sed -e "s/^ \([0-9]\+\)/\n $HIGHLIGHT\1$NORMAL/g"
        echo
        echo -e " \033[1;37mType \033[1;34m!<number>\033[1;37m to recall a command\033[0m"
        echo
