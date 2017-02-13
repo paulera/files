a44b91b6eba84595eeccbe09069029a9af5bb3a2
Paulo Amaral <paulo.amaral@gmail.com>
2017-02-13 10:47:13 +0000

    MacOS aliases

---
+++ bash.d/02_alias_macos.sh
@@ -0,0 +1,7 @@
+###############################
+# ALIASES EXXCLUSIVE FO MACOS #
+###############################
+
+# lock computer
+alias lock='/System/Library/CoreServices/"Menu Extras"/User.menu/Contents/Resources/CGSession -suspend'
+
