757dc8d57a01bf0bd14616b10645df26319967c9
Paulo Amaral <paulo.amaral@gmail.com>
2017-02-13 10:46:54 +0000

    git aliases

--- bash.d/02_alias.sh
+++ bash.d/02_alias.sh
@@ -27,3 +27,16 @@ alias rot13="tr '[A-Za-z]' '[N-ZA-Mn-za-m]'"
 
 # replace gs (GhostScript) with git status function. Escape \gs to run the original command.
 alias gs="$FILESROOT/bin/gs"
+
+# git commamands simplified (credit: http://blog.taylormcgann.com/2012/06/13/customize-your-shell-command-prompt)
+alias gst='git status'
+alias gco='git checkout'
+alias gci='git commit'
+alias grb='git rebase'
+alias gbr='git branch'
+alias gad='git add -A'
+alias gpl='git pull'
+alias gpu='git push'
+alias glg='git log --date-order --all --graph --format="%C(green)%h%Creset %C(yellow)%an%Creset %C(blue bold)%ar%Creset %C(red bold)%d%Creset%s"'
+alias glg2='git log --date-order --all --graph --name-status --format="%C(green)%H%Creset %C(yellow)%an%Creset %C(blue bold)%ar%Creset %C(red bold)%d%Creset%s"'
+
