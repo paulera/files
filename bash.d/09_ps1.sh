1f4ea2b60e81c257b656b892b6d06075898b0523
Paulo Amaral <paulo.amaral@gmail.com>
2017-05-17 15:32:17 +0100

    disabled error and job indicator in PS1

--- bash.d/09_ps1.sh
+++ bash.d/09_ps1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-JOBS="if [ ! \$(jobs | wc -l) == 0 ]; then echo \[\e[01\;30m\]\\\[\$(jobs | wc -l)\\\]\\[\e[0m\]\ ; fi"
-ERROR="if [ ! \$? = 0 ]; then echo \[\e[01\;31m\]!\[\e[0m\]; fi;"
+#JOBS="if [ ! \$(jobs | wc -l) == 0 ]; then echo \[\e[01\;30m\]\\\[\$(jobs | wc -l)\\\]\\[\e[0m\]\ ; fi"
+#ERROR="if [ ! \$? = 0 ]; then echo \[\e[01\;31m\]!\[\e[0m\]; fi;"
 
 export GIT_PS1_SHOWDIRTYSTATE=1
 
