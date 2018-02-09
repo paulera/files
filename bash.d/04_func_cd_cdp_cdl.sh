5363d19ad874cffd2441b2e8e682d389bb3813ab
Paulo Amaral <paulo.amaral@gmail.com>
2018-02-09 14:27:04 +0100

    cd, cdp and cdl: navigate with history through directories

---
+++ bash.d/04_func_cd_cdp_cdl.sh
@@ -0,0 +1,23 @@
+cd() {
+    if [ -z "$1" ]; then
+        pushd ~ &> /dev/null
+    else
+        
+        pushd "$*" &> /dev/null
+    fi
+}
+
+cdp() {
+    popd &> /dev/null
+}
+
+cdl() {
+    if [[ $1 =~ ^[0-9]+$ ]] ; then
+        pushd +$1 &> /dev/null
+    else
+        echo
+        dirs -v -p | tail -n +2
+        echo
+    fi
+}
+
