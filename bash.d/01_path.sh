9aaeef59bd2af5839b529ce4d28da8d5aea3ea27
Paulo Amaral <paulo.amaral@gmail.com>
2021-04-26 15:25:00 +0200

    gave maximum precedence for ~/bin in PATH

--- bash.d/01_path.sh
+++ bash.d/01_path.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-export PATH=$PATH:"$HOME/bin:"
-export PATH=$PATH:"$FILESROOT/bin:"
-export PATH=$PATH:"$FILESROOT/soft/bin:"
+export PATH="$PATH:$FILESROOT/bin:"
+export PATH="$PATH:$FILESROOT/soft/bin:"
+export PATH="$HOME/bin:$PATH:"
