a09c15503146ed123db3fed96150227a7d7f1168
Paulo Amaral <paulo.amaral@gmail.com>
2022-02-24 12:12:55 +0100

    added suport through exa settings EXA_COLORS during bash.d 02

--- bash.d/02_dircolors.sh
+++ bash.d/02_dircolors.sh
@@ -1,3 +1,6 @@
 if [ -f $FILESROOT/dircolors ]; then
 	eval "`dircolors -b $FILESROOT/dircolors`"
+
+	# additional variable to set exa tool colours to match the system ones
+	export EXA_COLOR=$LS_COLORS
 fi
