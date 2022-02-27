0c692f3a5acfd058712f0fd450db3722192c3582
Paulo Amaral <paulo.amaral@gmail.com>
2022-02-27 15:42:50 +0100

    Added [tor] indicator to PS1

--- bash.d/09_ps1.sh
+++ bash.d/09_ps1.sh
@@ -2,39 +2,80 @@
 
 #JOBS="if [ ! \$(jobs | wc -l) == 0 ]; then echo \[\e[01\;30m\]\\\[\$(jobs | wc -l)\\\]\\[\e[0m\]\ ; fi"
 #ERROR="if [ ! \$? = 0 ]; then echo \[\e[01\;31m\]!\[\e[0m\]; fi;"
+#S=""
 
 export GIT_PS1_SHOWDIRTYSTATE=1
 
 newps() {
-	
+
 	c() {
-	    echo "\\[\e[$1m\\]"
+	    echo -n "\\[\e[$1m\\]"
 	}
-	
-	if [ "_$(id -u)" == "_0" ]; then
+
+	reset() {
+	    echo -n $(c '0')
+	}
+
+	red() {
+        echo -n $(c '1;31')$1$(c '0')
+	}
+
+    green() {
+        echo -n $(c '1;32')$1$(c '0')
+    }
+
+    blue() {
+        echo -n $(c '1;34')$1$(c '0')
+    }
+
+    white() {
+        echo -n $(c '1;37')$1$(c '0')
+    }
+
+    purple() {
+        echo -n $(c '0;35')$1$(c '0')
+    }
+
+    purpleinv() {
+        echo -n $(c '45;30')$1$(c '0')
+    }
+
+    ps1_tor() {
+        [[ $LD_PRELOAD == *"torsocks/libtorsocks"* ]] && echo "[tor] "
+    }
+
+    if [ "$(id -u)" -eq 0 ]; then
 		# root
 export PS1="\
-$(c '1;31')\u@\h$(c '0') \
-$(c '1;34')\w$(c '0') \
+$(red \\u@\\h) \
+$(blue \\w) \
 \\$ "
 	else
 		# common
         if [ "$1" == "local" ]; then
 export PS1="\
-$(c '1;37')\$(__git_ps1 '(%s)\\[\e[0m\\] ')$(c '0')\
-$(c '1;34')\w$(c '0') \
+$(purple "\$(ps1_tor)")\
+$(white "\$(__git_ps1 '(%s) ')")\
+$(blue \\w) \
 \\$ "
         else
 export PS1="\
-$(c '1;32')\u@\h$(c '0') \
-$(c '1;37')\$(__git_ps1 '(%s)\\[\e[0m\\] ')$(c '0')\
-$(c '1;34')\w$(c '0') \
+$(purple "\$(ps1_tor)")\
+$(green \\u@\\h) \
+$(white "\$(__git_ps1 '(%s) ')")\
+$(blue \\w) \
 \\$ "
         fi
 		
 	fi
 }
 newps
+
+# old piece for git status
+# $(c '1;37')\$(__git_ps1 '(%s)\\[\e[0m\\] ')$(c '0')\
+
+# working tor bit
+# `[[ $LD_PRELOAD == *"torsocks/libtorsocks"* ]] && echo "$(purpleinv \"tor\")$(purple $S) ";`\
 
 #emoji idea - parked
 #if [[ "$(uname -s)" == CYGWIN* ]]; then
