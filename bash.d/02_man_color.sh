b4de56d3bb906afbf337c64bfd148db48b5ef8ec
Paulo Amaral <paulo.amaral@gmail.com>
2022-03-15 14:01:56 +0100

    Better colours for less/manwq

--- bash.d/02_man_color.sh
+++ bash.d/02_man_color.sh
@@ -1,8 +1,18 @@
 # Less Colors for Man Pages
-export LESS_TERMCAP_mb=$'\E[01;31m'        # begin blinking
-export LESS_TERMCAP_md=$'\E[00;34m'        # begin bold
-export LESS_TERMCAP_me=$'\E[0m'            # end mode
-export LESS_TERMCAP_se=$'\E[0m'            # end standout-mode
-export LESS_TERMCAP_so=$'\E[01;44;33m'     # begin standout-mode - info box
-export LESS_TERMCAP_ue=$'\E[0m'            # end underline
-export LESS_TERMCAP_us=$'\E[00;32m'        # begin underline
+
+# Source: https://unix.stackexchange.com/questions/119/colors-in-man-pages
+
+export LESS_TERMCAP_mb=$(tput bold; tput setaf 2) # green
+export LESS_TERMCAP_md=$(tput bold; tput setaf 6) # cyan
+export LESS_TERMCAP_me=$(tput sgr0)
+export LESS_TERMCAP_so=$(tput bold; tput setaf 3; tput setab 4) # yellow on blue
+export LESS_TERMCAP_se=$(tput rmso; tput sgr0)
+export LESS_TERMCAP_us=$(tput smul; tput bold; tput setaf 7) # white
+export LESS_TERMCAP_ue=$(tput rmul; tput sgr0)
+export LESS_TERMCAP_mr=$(tput rev)
+export LESS_TERMCAP_mh=$(tput dim)
+export LESS_TERMCAP_ZN=$(tput ssubm)
+export LESS_TERMCAP_ZV=$(tput rsubm)
+export LESS_TERMCAP_ZO=$(tput ssupm)
+export LESS_TERMCAP_ZW=$(tput rsupm)
+export GROFF_NO_SGR=1         # For Konsole and Gnome-terminal
