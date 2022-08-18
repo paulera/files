3edef01305f3b45e8f2d8a82c6f864917a6ba0b4
Paulo Amaral <paulo.amaral@gmail.com>
2022-08-18 17:02:23 +0200

    improves ffa: linebreaks for readability, exclude .git, vendor, node_modules

--- bash.d/04_func_ffa.sh
+++ bash.d/04_func_ffa.sh
@@ -1,5 +1,12 @@
 # Extracting
 ffa () {
-	grep --color=always -B2 -F2 -i -n -H -a -R "$*" * &> /dev/stdout | grep -v "^grep: .*$\|^Binary file \(standard input\) matches$"
+	grep \
+	    --color=always \
+	    --exclude-dir=".git" \
+	    --exclude-dir="vendor" \
+	    --exclude-dir="node_modules" \
+	    -B2 -F2 \
+	    -i -n -H -a -R "$*" * &> /dev/stdout | \
+	    grep -v "^grep: .*$\|^Binary file \(standard input\) matches$"
 }
 
