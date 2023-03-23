7fd32943700f2eaf59aa1ee0ebff14b1c5b675a1
Paulo Amaral <paulo.amaral@gmail.com>
2023-03-23 14:35:54 +0100

    Update ffa.sh

--- bash.d/04_functions/ffa.sh
+++ bash.d/04_functions/ffa.sh
@@ -3,13 +3,24 @@
 # This function ignores folders where you are unlikely to be
 # searching stuff, like vendor and configuration ones.
 
-ffa () {
-	grep \
-	    --color=always \
-	    --exclude-dir=".git" \
-	    --exclude-dir="vendor" \
-	    --exclude-dir="node_modules" \
-	    -B2 -F2 \
-	    -i -n -H -a -R -I "$*" * &> /dev/stdout
-}
+# The list of files will be saved in /tmp/files_that, to allow ease reuse of
+# results with the function `that`
 
+ffa () {
+        grep \
+            --color=always \
+            --exclude-dir=".git" \
+            --exclude-dir="vendor" \
+            --exclude-dir="node_modules" \
+            -B2 -F2 \
+            -i -n -H -a -R -I "$*" * &> /dev/stdout | tee /tmp/files_that
+
+        # remove colors
+        sed -r -i 's/\x1B\[[0-9;]*[JKmsu]//g' /tmp/files_that
+
+        # leave only occurence lines
+        perl -i -ne 'print if /:[0-9]+:/' /tmp/files_that
+
+        # leaves only the filename
+        sed -r -i 's/:.*$//g' /tmp/files_that
+}
