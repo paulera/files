3d68bca50e15daae32db7449a81d7f5d29d618c4
Paulo Amaral <paulo.amaral@gmail.com>
2023-04-13 01:43:58 +0200

    made locale setting dynamic: detects what is the utf8 locale available

--- bash.d/00_vars_and_options/locale_options.sh
+++ bash.d/00_vars_and_options/locale_options.sh
@@ -1,6 +1,15 @@
 # Set the locale options to UTF-8
 
-export LANGUAGE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-export LC_TYPE=en_US.UTF-8
+ENCODING=$(locale -a | grep -i utf8)
+
+if [ ! -z "$ENCODING" ]; then
+    export LANGUAGE=${ENCODING}
+    export LC_ALL=${ENCODING}
+    export LANG=${ENCODING}
+    export LC_TYPE=${ENCODING}
+fi
+
+#export LANGUAGE=en_US.UTF-8
+#export LC_ALL=en_US.UTF-8
+#export LANG=en_US.UTF-8
+#export LC_TYPE=en_US.UTF-8
