68d4e8878c2fcc6128aaa8ee33371ce089fcffa6
Paulo Amaral <paulo.amaral@gmail.com>
2024-04-06 01:40:01 +0200

    gitp added to FILESw

---
+++ bin/gitp.sh
@@ -0,0 +1,12 @@
+function gitp(){
+    
+    GITP_KEY="~/.ssh/personal-id_rsa"
+
+    if [[ "$1" =~ ^(hello|hi|yo|test|check|dry)$ ]]; then
+        ssh -i ~/.ssh/personal-id_rsa -T git@github.com
+    else
+        ssh -i /Users/pamaral/.ssh/personal-id_rsa -o IdentitiesOnly=yes git $*
+    fi
+}
+
+alias gitp2="GIT_SSH_COMMAND='ssh -i ~/.ssh/personal-id_rsa -o IdentitiesOnly=yes' git"
