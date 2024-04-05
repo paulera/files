4925fb933fe28da61fc70dcac592853f7008f28b
Paulo Amaral <paulo.amaral@gmail.com>
2024-04-06 01:22:19 +0200

    ~/bin and FILES bin take precedence

--- bash.d/01_path/path.sh
+++ bash.d/01_path/path.sh
@@ -1,9 +1,12 @@
-# FILES repo scripts
-export PATH="$PATH:$FILESROOT/bin:"
+# Manipulation of PATH
+# from less to more priority
 
 # FILES repo soft binaries
 # TODO: do I really need soft binaries?
-export PATH="$PATH:$FILESROOT/soft/bin:"
+export PATH="$FILESROOT/soft/bin:$PATH:"
+
+# FILES scripts
+export PATH="$FILESROOT/bin:$PATH:"
 
 # User bin folder at ~/bin
 export PATH="$HOME/bin:$PATH:"
