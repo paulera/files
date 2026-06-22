657c058b26b752338767383b4e01a3f08d3f0bd4
Paulo Amaral <paulo.amaral@gmail.com>
2026-06-22 17:34:42 +0200

    extract: support files with space in the name

--- bash.d/04_functions/extract.sh
+++ bash.d/04_functions/extract.sh
@@ -1,8 +1,8 @@
 # Function for extracting files
 # It will call the right extraction command based on the file extension
 extract () {
-  if [ -f $1 ] ; then
-      case $1 in
+  if [ -f "${1}" ] ; then
+      case "${1}" in
           *.tar.bz2)   tar xvjf $1    ;;
           *.tar.gz)    tar xvzf $1    ;;
           *.bz2)       bunzip2 $1     ;;
@@ -11,7 +11,7 @@ extract () {
           *.tar)       tar xvf $1     ;;
           *.tbz2)      tar xvjf $1    ;;
           *.tgz)       tar xvzf $1    ;;
-          *.zip)       unzip $1       ;;
+          *.zip)       unzip "$1"     ;;
           *.Z)         uncompress $1  ;;
           *.7z)        7z x $1        ;;
           *)           echo "I don't know how to extract $1" ;;
